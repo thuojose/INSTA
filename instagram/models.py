2455543ba8d5b7459a7eade80e5dadafbf797a2c
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-06 23:48:53 +0300

    added class model for image

--- instagram/models.py
+++ instagram/models.py
@@ -37,3 +37,49 @@ def create_profile(sender, instance,created,**kwargs):
 @receiver(post_save,sender=User)
 def save_profile(sender, instance,**kwargs):
     instance.profile.save()
+
+#Image Model
+class Image(models.Model):
+    image = ImageField(manual_crop="")
+    name = models.CharField(max_length =70)
+    caption = models.CharField(max_length =700)
+    post_date = models.DateTimeField(auto_now_add=True)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
+
+    
+    def save_image(self):
+        self.save()
+            
+    def update_image(self):
+        img=Image.objects.filter(id=Image.id).update()
+        
+    def update_caption(self):
+        img=Image.objects.filter(id=Image.id).update()     
+        
+    def delete_image(self):
+        # img = get_object_or_404(Image,pk=Image.id)
+        img=Image.objects.filter(id=Image.id).delete()
+   
+    class Meta:
+        ordering = ['post_date']
+    
+    @classmethod
+    def images(cls):
+        images = cls.objects.filter(id=Image.id)
+        return images
+    
+    
+    @classmethod
+    def search_by_username(cls,username):
+        fetch_user= User.objects.get(username=username)
+        foundUser = cls.objects.filter(profile=fetch_user)
+        return foundUser
+    
+    @classmethod
+    def filter_by_user_id(cls):
+        photos = cls.objects.filter(id=user.user_id)
+        return photos
+    
+    def __str__(self):
+        return str(self.name)
