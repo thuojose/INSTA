81a42b0d19e4e20c743aed65f9de25570b4c6863
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-06 23:28:27 +0300

    added function for profile

--- instagram/views.py
+++ instagram/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render,redirect,get_object_or_404
-from django.contrib.auth import login
+from django.contrib.auth import login,views, forms
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse,Http404,HttpResponseRedirect
 from .forms import LikesForm, CommentsForm
+from django.core.exceptions import ObjectDoesNotExist
+from .models import *
+from django.contrib.auth.models import User
+from .models import Image
+
+
 
 # Create your views here.
 #landing page
@@ -93,3 +99,18 @@ def search_results(request):
     else:
         message = "You haven't searched for any term"
         return render(request, 'instagram_pages/search.html',locals())
+
+#profile page
+@login_required(login_url='/accounts/login')
+def profilePage(request):
+    likesForm = LikesForm
+    commentForm = CommentsForm
+    images = Image.objects.all()
+    user = request.user.get_username()
+    current_user = request.user
+    photos = Image.objects.filter(profile=current_user.id)
+    profile = Profile.objects.all()
+    likes = Like.objects.all()
+ 
+    
+    return render(request,'instagram_pages/profile.html', locals())
