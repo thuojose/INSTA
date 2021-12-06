c52265637b3b6254a9045b524cbe21bb9bb02f80
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-06 23:56:12 +0300

    added url for uploadpic/update profile

--- instagram/urls.py
+++ instagram/urls.py
@@ -11,6 +11,10 @@ urlpatterns = [
     path('comment/<int:image_id>',views.comments,name='comments'),
     path('search/', views.search_results,name='searchresults'),
     path('profile/',views.profilePage,name='instagramProfile'),
+    path('upload/',views.uploadPic,name='instagramFeed'),
+    path('update-profile/',views.updateProfile,name='editProfile'),
+    
+    
      ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
