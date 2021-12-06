2209b3e1ccacf4ac4a9489b836389eb4863ad2c0
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-06 23:08:20 +0300

    added url for like/comment

--- instagram/urls.py
+++ instagram/urls.py
@@ -7,7 +7,8 @@ from django.urls import path
 urlpatterns = [
     path('',views.landing,name='instagramLandingPage'),
     path('home/',views.home,name='instagramHome'),
-    
+     path('like/<int:image_id>',views.likes,name='likes'),
+    path('comment/<int:image_id>',views.comments,name='comments'),
        ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
