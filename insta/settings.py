9184836f004a2cb680d3369baf6d9280abb3579b
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-06 23:25:14 +0300

    installion and setting up of pyuploadcare

--- insta/settings.py
+++ insta/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'instagram',
     'bootstrap4',
+    'pyuploadcare.dj',
+    
 ]
 
 MIDDLEWARE = [
@@ -68,6 +71,12 @@ TEMPLATES = [
         },
     },
 ]
+
+from decouple import config
+UPLOADCARE = {
+    'pub_key':'893f810fd99ee6eb9583',
+    'secret':config('SECRET'),
+}
 
 WSGI_APPLICATION = 'insta.wsgi.application'
 
@@ -107,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
@@ -129,3 +138,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+LOGIN_REDIRECT_URL = '/home'
+
+LOGOUT_REDIRECT_URL = 'instagramLandingPage'
