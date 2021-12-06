50f13eaeeb8c53d73c477f4c5ee629a23eee3951
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-06 22:44:44 +0300

    added homepage function in view.py

--- instagram/views.py
+++ instagram/views.py
@@ -8,3 +8,21 @@ from django.http import HttpResponse,Http404,HttpResponseRedirect
 def landing(request):
     form = forms.AuthenticationForm
     return render(request, 'landing_page.html', locals())
+
+#home page
+@login_required(login_url='/accounts/login')
+def home(request):
+    likesForm = LikesForm
+    commentForm = CommentsForm
+    images = Image.objects.all()
+    user = request.user.get_username()
+    profile = Profile.objects.all()
+    likes = Like.objects.all()
+    numberOfLikes = len(likes)
+    comments = Comment.get_comments(id)
+    comments = Comment.objects.all()
+    numberOfComments=len(comments)
+    commentsPerImage = Comment.objects.filter(image_id=images.id).all()
+    
+    
+    return render(request,'instagram_pages/home.html', locals())
