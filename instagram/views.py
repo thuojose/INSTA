7d7134cfc7aef89289e104c418cb56a644ab4ab6
KHALYZ <joseph.thuo@student.moringaschool.com>
2021-12-06 22:27:56 +0300

    created login landing page

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 
 # Create your views here.
+#landing page
+def landing(request):
+    form = forms.AuthenticationForm
+    return render(request, 'landing_page.html', locals())
